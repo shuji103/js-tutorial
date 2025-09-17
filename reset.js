74d101e18fa55d19066425b8dd3b0dd37fb4f533
shuji103 <shuji6927@gmail.com>
2025-09-18 01:12:07 +0900

    メッセージ初期化とボタンの再度有効化機能を追加

--- reset.js
+++ reset.js
@@ -2,9 +2,20 @@
 
 (() => {
     const $counter = document.getElementById("js-counter");
+    const $message = document.getElementById("js-message");
 
     const clickHandler = () => {
+        // カウント部分の初期化
         $counter.textContent = 0;
+
+        // メッセージ初期化
+        $message.innerText = "You're down!";
+
+        // 無効化していたボタンを再度有効化
+        const $buttons = document.getElementsByClassName("js-button");
+        for (let index = 0; index < $buttons.length; index++) {
+            $buttons[index].disabled = false;
+        }
     }
 
     document.getElementById("js-reset-button").addEventListener("click", clickHandler)
